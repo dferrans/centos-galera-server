396fa72a6dd3d463b2722a5d646ea072543cdfd2
root <root@gitsvr.localdomain>
2016-05-26 19:06:58 -0400

    First commit base

---
+++ provision.sh
@@ -0,0 +1,14 @@
+ #!/bin/bash          
+echo this script will help to install al dependencies and packages to create a galera node
+echo let us start    
+#
+yum install screen epel-release -y
+touch /etc/yum.repos.d/mariadb.repo
+cat mariadbrepo.txt > /etc/yum.repos.d/mariadb.repo
+yum --enablerepo=mariadb -y install MariaDB-Galera-server
+systemctl start mysql
+mysql_secure_installation
+/etc/rc.d/init.d/mysql bootstrap 
+/etc/rc.d/init.d/mysql bootstrap 
+yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
+

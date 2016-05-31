203c10021bc0559e9872509847a32c40a9ee4f38
dferrans <diferrans@gmail.com>
2016-05-31 07:41:47 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -6,9 +6,10 @@ yum install screen epel-release -y
 touch /etc/yum.repos.d/mariadb.repo
 cat mariadbrepo.txt > /etc/yum.repos.d/mariadb.repo
 yum --enablerepo=mariadb -y install MariaDB-Galera-server
-systemctl start mysql
-mysql_secure_installation
-/etc/rc.d/init.d/mysql bootstrap 
-/etc/rc.d/init.d/mysql bootstrap 
+#systemctl start mysql
+#mysql_secure_installation
+#/etc/rc.d/init.d/mysql bootstrap 
+#/etc/rc.d/init.d/mysql bootstrap 
 yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
+echo instalacion basica lista
 
